2a4e7cc0090e23f41431406d7a8afb08460544a6
Louis Roy <louisroy@louisroy.net>
2014-10-28 14:20:04 -0400

    more more stuff

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,20 +1,31 @@
-var container = document.querySelector('.works');
-var works = document.querySelectorAll('.work');
-/*
-for (i = 0; i < works.length; ++i) {
-	var image = works[i].querySelector('img');
-	
-	works[i].style.backgroundImage = 'url(' + image.src + ')';
-}
-*/
+var $container = document.querySelector('.works');
+var $works = $('.work');
 
-var pckry = new Packery(container, {
+var pckry = new Packery($container, {
   // options
   itemSelector: '.work',
-  gutter:'.gutter-sizer'
+  gutter:'.gutter-sizer',
+  transitionDuration:'0s'
 });
 
-$(works).find('a').fancybox({
+$works.find('a[rel=work]').fancybox({
 	scrolling:'no',
 	arrows:true
 });
+
+window.onhashchange = function(ev) {
+	var category = window.location.hash.replace(/#/, '');
+	
+	if (category) {
+		$works.hide();
+		$works.filter('[data-category*="' + category + '"]').show();
+	} else {
+		$works.show();
+	}
+	
+	pckry.layout();
+	
+	$('.nav-works a').removeClass('active').filter('[href="#'  + category + '"]').addClass('active');
+};
+
+window.onhashchange();
