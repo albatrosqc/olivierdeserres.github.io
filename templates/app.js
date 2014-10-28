93b395ee67dbe4e3bd9bef18d0a71a6e57c6cd56
Louis Roy <louisroy@louisroy.net>
2014-10-28 10:40:03 -0400

    stuff

--- templates/app.js
+++ templates/app.js
@@ -1,8 +1,20 @@
+var container = document.querySelector('.works');
 var works = document.querySelectorAll('.work');
-
+/*
 for (i = 0; i < works.length; ++i) {
 	var image = works[i].querySelector('img');
 	
 	works[i].style.backgroundImage = 'url(' + image.src + ')';
-	console.log(image.src);
 }
+*/
+
+var pckry = new Packery(container, {
+  // options
+  itemSelector: '.work',
+  gutter:'.gutter-sizer'
+});
+
+$(works).find('a').fancybox({
+	scrolling:'no',
+	arrows:true
+});
