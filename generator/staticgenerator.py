262cfd29d688ac5a1b5d1df3970c984104b0d640
jg-you <jean.gabriel.young@gmail.com>
2014-10-29 13:25:41 -0400

    Dropbox scanner = done

--- generator/staticgenerator.py
+++ generator/staticgenerator.py
@@ -2,19 +2,58 @@
 
 from jinja2 import Environment, FileSystemLoader
 import os
-import shutil
+import yaml
 
 class StaticGenerator :
-    def __init__(self, dropboxPath):
+    """
+        Generate html content from a Dropbox folder.
+    """
+
+    # Data members
+    tree=dict()
+    dropboxPath=""
+
+    def __init__(self, dropboxPath) :
         """
             Constructor: initialize with the absolute path to the DropBox folder.
         """
         self.dropboxPath = dropboxPath
 
-    def directoryScanner
+    def BuildTree(self,currentPath) :
+        """ 
+            Recursive directory tree builder.
+        """
 
-        baseDirectoryContent = os.listdir(self.dropboxPath)
+        contentList = os.listdir(currentPath)
+        fileList = []
 
-        for dirName in baseDirectoryContent :
-            self.tree[dirName] = os.listdir(self.dropboxPath+dirName)
+        for content in contentList :
+            path = os.path.join(currentPath,content)
+            if os.path.isdir(path) :
+                self.BuildTree(path)
+            else :
+                fileList.append(content)
 
+        self.tree[currentPath[len(self.dropboxPath):]+"/"]=fileList
+
+    def ScanDirectory(self) :
+        self.BuildTree(self.dropboxPath)
+        for directory in self.tree :
+            for f in self.tree[directory]:
+                path=self.dropboxPath+directory+f
+                return name, content = self.ReadFile(path)
+
+    def ReadFile(self,filePath) :
+        """ 
+            Determine if a file contains YAML.
+            If true, read and return content as a dictionary + file type.
+        """
+        
+        directory, fileName = os.path.split(filePath)
+        name, extension = os.path.splitext(fileName)
+
+        if extension == ".yaml" or  extension == ".yml" :
+            f = open(filePath,"r")
+            return name, yaml.load(f.read())
+
+        return name, {"Files":None} 
