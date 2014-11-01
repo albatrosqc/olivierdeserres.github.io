e2fc2449f385fc16afa7c3dc3c0dcd207498b38e
Louis Roy <louisroy@louisroy.net>
2014-11-01 13:18:43 -0400

    refactored code, added comments

--- design/js/app.js
+++ design/js/app.js
@@ -1,63 +1,96 @@
-var $container = $('.works');
-var $works = $('.work');
-
-$container.packery({
-  // options
-  itemSelector: '.work',
-  gutter:'.gutter-sizer',
-  transitionDuration:'0s'
-});
-
-imagesLoaded($container, function(ev) {
-	$container.packery('layout');
-});
-
-$works.find('a[rel=work]').fancybox({
-	scrolling:'no',
-	arrows:true
-});
-
-$('.nav-works a').click(function(ev) {
-	var $link = $(this);
+var App = (function() {
+	// App variables
+	var $container = $('.works');
+	var $works = $('.work');
+	var $navWorks = $('.nav-works');
 	
-	$link.closest('.nav-works').find('a').removeClass('active');
-	$link.addClass('active');
+	// Navigation event
 	
-	ev.preventDefault();
-	
-	var $selectors = $('.nav-works a.active');
-	var params = {};
-	
-	$selectors.each(function() {
-		params[$(this).closest('.nav-works').data('type')] = $(this).attr('href').replace(/#/, '');
-	});
-	
-	// TODO : disable links when no results
-	
-	window.location.hash = $.param(params);
-});
-
-window.onhashchange = function(ev) {
-	var params = $.deparam(window.location.hash.replace(/#/, ''));
-	
-	var filters = [];
-	
-	$.each(params, function(key, value) {
-		$('.nav-works[data-type="' + key + '"] a').removeClass('active').filter('[href="#'  + value + '"]').addClass('active');
+	var onNavWorksClick = function(ev) {
+		// Prevent link from firing, hash will be changed further down
+		ev.preventDefault();
 		
-		if (value) {
-			filters.push('[data-' + key +'*="' + value + '"]');
+		var $link = $(this);
+		
+		// Apply active class on link
+		$link.closest('.nav-works').find('a').removeClass('active');
+		$link.addClass('active');
+		
+		// TODO : disable links when no results
+		
+		// Prepare parameters
+		var $selectors = $('.nav-works a.active');
+		var params = {};
+		
+		// Loop through active links and build parameters
+		$selectors.each(function() {
+			params[$(this).closest('.nav-works').data('type')] = $(this).attr('href').replace(/#/, '');
+		});
+		
+		// Store paramaters in the hash
+		window.location.hash = $.param(params);
+	};
+	
+	// Hash change event
+	
+	var onHashChange = function(ev) {
+		// Fetch parameters from hash
+		var params = $.deparam(window.location.hash.replace(/#/, ''));
+		
+		// Setup filters
+		var filters = [];
+		
+		// Loop through parameters
+		$.each(params, function(key, value) {
+			// Activate link in navigation
+			$('.nav-works[data-type="' + key + '"] a').removeClass('active').filter('[href="#'  + value + '"]').addClass('active');
+			
+			// Add filter if value is not null
+			if (value) {
+				filters.push('[data-' + key +'*="' + value + '"]');
+			}
+		});
+		
+		if (filters.length) {
+			// If we have filters, hide all works and show only relevant ones
+			$works.hide();
+			$works.filter(filters.join('')).show();
+		} else {
+			// Show all works if we have no filters
+			$works.show();
 		}
-	});
+		
+		// Repack all works in a new layout
+		$container.packery('layout');
+		
+		// TODO : reset fancybox with only visible ones
+	};
 	
-	if (filters.length) {
-		$works.hide();
-		$works.filter(filters.join('')).show();
-	} else {
-		$works.show();
-	}
+	// Constructor
 	
-	$container.packery('layout');
-};
-
-window.onhashchange();
+	var construct = (function() {
+		// Layout all the works using packery
+		$container.packery({
+			itemSelector: '.work', // Item selector
+			gutter:'.gutter-sizer', // Percent-based gutter
+			transitionDuration:'0s' // No transition when resizing
+		});
+		
+		// When all images are loaded, apply new layout to make sure everything looks good
+		imagesLoaded($container, function(ev) {
+			$container.packery('layout');
+		});
+		
+		// Setup fancybox for all the works
+		$works.find('a[rel=work]').fancybox({
+			scrolling:'no',
+			arrows:true
+		});
+		
+		// Click event on work navigation
+		$navWorks.find('a').on('click', onNavWorksClick);
+		
+		// Hash change event
+		$(window).bind('hashchange', onHashChange).trigger('hashchange');
+	})();
+})();
